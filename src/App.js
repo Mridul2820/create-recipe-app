2992ab632dfe9ae35fa74a8bfcab221243434cf7
Mridul2820 <immridulpanda2820@gmail.com>
2020-12-27 20:28:04 +0530

    recipe list added and components breakdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React, { useState } from 'react';
-
-export const ThemeContext = React.createContext();
+import React from 'react';
+import RecipeList from './RecipeList'
 
 function App() {
-    return null;
+    return (
+        <RecipeList recipes={sampleRecipes}/>
+    )
 }
+
+const sampleRecipes = [
+    {
+        id: 1,
+        name: "Plain Chicken",
+        servings: 3,
+        cookTime: "1:45",
+        instructions : "1. Put Salt on Chicken\n2. Put Chicken in Ovan\n3. Eat Chicken"
+    },
+    {
+        id: 2,
+        name: "Plain Rice",
+        servings: 5,
+        cookTime: "0:45",
+        instructions : "1. Put Rice on Water\n2. Boil the rice with water in Ovan\n3. Eat Rice"
+    }
+]
 
 export default App;
