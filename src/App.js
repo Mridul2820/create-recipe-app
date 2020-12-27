8fca2124e9b9467ae63dd9c575547cd6cb40913d
Mridul2820 <immridulpanda2820@gmail.com>
2020-12-27 21:23:59 +0530

    ingredients component added

--- src/App.js
+++ src/App.js
@@ -13,14 +13,48 @@ const sampleRecipes = [
         name: "Plain Chicken",
         servings: 3,
         cookTime: "1:45",
-        instructions : "1. Put Salt on Chicken\n2. Put Chicken in Ovan\n3. Eat Chicken"
+        instructions : "1. Put Salt on Chicken\n2. Put Chicken in Ovan\n3. Eat Chicken",
+        ingredients: [
+            {
+                id: 1,
+                name: 'Chicken',
+                amount: '500 gm'
+            },
+            {
+                id: 2,
+                name: 'Salt',
+                amount: '2 tbs'
+            },
+            {
+                id: 3,
+                name: 'Potato',
+                amount: '200 gm'
+            },
+            {
+                id: 4,
+                name: 'Garam Mashala',
+                amount: '1 Packet'
+            }
+        ]
     },
     {
         id: 2,
         name: "Plain Rice",
         servings: 5,
         cookTime: "0:45",
-        instructions : "1. Put Rice on Water\n2. Boil the rice with water in Ovan\n3. Eat Rice"
+        instructions : "1. Put Rice on Water\n2. Boil the rice with water in Ovan\n3. Eat Rice",
+        ingredients: [
+            {
+                id: 1,
+                name: 'Rice',
+                amount: '1 KG'
+            },
+            {
+                id: 2,
+                name: 'Water',
+                amount: '1 Liter'
+            }
+        ]
     }
 ]
 
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import IngredientList from './IngredientList';
 
 export default function Recipe(props) {
-    const { name, cookTime, servings, instructions } = props;
+    const { 
+        name, 
+        cookTime, 
+        servings, 
+        instructions, 
+        ingredients 
+    } = props; 
     
     return (
         <div>
@@ -23,6 +30,13 @@ export default function Recipe(props) {
             <div>
                 <span>Instructions : </span>
                 <div>{instructions} </div>
+            </div>
+
+            <div>
+                <span>Ingredients : </span>
+                <div>
+                    <IngredientList ingredients={ingredients} />
+                </div> 
             </div>
         </div>
     )
