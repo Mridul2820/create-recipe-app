e7473c66b535b0f51313a89d3f6632294a23930b
Mridul2820 <immridulpanda2820@gmail.com>
2021-01-03 13:28:05 +0530

    Add recipe and delete recipe button works

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,11 +3,13 @@ import IngredientList from './IngredientList';
 
 export default function Recipe(props) {
     const { 
+        id,
         name, 
         cookTime, 
         servings, 
         instructions, 
-        ingredients 
+        ingredients ,
+        handleRecipeDelete
     } = props; 
     
     return (
@@ -16,23 +18,25 @@ export default function Recipe(props) {
                 <h3 className="recipe__title">{name}</h3>
                 <div>
                     <button className="btn btn--primary mr-1">Edit</button>
-                    <button className="btn btn--denger">Delete</button>
+                    <button className="btn btn--denger"
+                    onClick={() => handleRecipeDelete(id)}
+                    >Delete</button>
                 </div>
             </div>
             <div className="recipe__row">
                 <span className="recipe__label">Cook Time : </span>
                 <span className="recipe__value">{cookTime}</span>
             </div>
-            <div recipe__row>
+            <div className="recipe__row">
                 <span className="recipe__label">Servings : </span>
                 <span className="recipe__value">{servings}</span>
             </div>
-            <div recipe__row>
+            <div className="recipe__row">
                 <span className="recipe__label">Instructions : </span>
                 <div className="recipe__value recipe__instructions recipe__value--indented">{instructions} </div>
             </div>
 
-            <div recipe__row>
+            <div className="recipe__row">
                 <span className="recipe__label">Ingredients : </span>
                 <div className="recipe__value recipe__value--indented">
                     <IngredientList ingredients={ingredients} />
